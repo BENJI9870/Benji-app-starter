b0055ea4e878dd36b53cebd2b69f59109c2cf0fd
Benjamin Cohn <benjamincohn@Benjamins-MBP-2.ecfs.org>
2023-05-04 13:07:10 -0400

    home UI update

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -49,14 +51,13 @@ class HomePage extends StatelessWidget {
           final user = FirebaseAuth.instance.currentUser;
           if (user != null) {
             if (user.emailVerified) {
-              print('Email is verified');
+              return const NotesView();
             } else {
               return const VerifyEmailView();
             }
           } else {
             return const LoginView();
           }
-          return const Text('Done');
         } else {
           // Show a loading indicator while Firebase is initializing
           return const Scaffold(
@@ -66,6 +67,57 @@ class HomePage extends StatelessWidget {
           );
         }
       },
+    );
+  }
+}
+// class _AccountConfirmationState extends State<AccountConfirmation> {
+
+//     late Timer _timer;
+
+//     @override
+//     void initState() {
+//       super.initState();
+
+//       _timer = Timer.periodic(const Duration(seconds: 5), (timer) async {
+//       await FirebaseAuth.instance.currentUser?.reload();
+//       final user = FirebaseAuth.instance.currentUser;
+//         if (user?.emailVerified ?? false) {
+//           timer.cancel();
+//           Navigator.pop(context, true);
+//         }
+//       });
+//     }
+
+//     @override
+//     void dispose() {
+//       super.dispose();
+//       _timer.cancel();
+//     }
+
+//     @override
+//     Widget build(BuildContext context) {
+//       return const Scaffold(
+//             body: Center(
+//               child: CircularProgressIndicator(),
+//             ),
+//       );
+//     }
+
+// }
+class NotesView extends StatefulWidget {
+  const NotesView({super.key});
+
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
+
+class _NotesViewState extends State<NotesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main UI'),
+        ),
     );
   }
 }
